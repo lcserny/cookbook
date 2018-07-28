11707e1e3355c9add5d2b0f03616b92ee84d041e
Leonardo Cserny <leonardo_cserny@netex.ro>
2018-07-28 17:24:16 +0300

    adds cmdargs

---
+++ chapter2/cmdargs/example/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"cookbook/chapter2/cmdargs"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	c := cmdargs.MenuConf{}
+	menu := c.SetupMenu()
+	menu.Parse(os.Args[1:])
+
+	// we use arguments to switch between commands
+	// flags are also an argument
+	if len(os.Args) > 1 {
+		// we don't care about case
+		switch strings.ToLower(os.Args[1]) {
+		case "version":
+			c.Version()
+		case "greet":
+			f := c.GetSubMenu()
+			if len(os.Args) < 3 {
+				f.Usage()
+				return
+			}
+
+			if len(os.Args) > 3 {
+				f.Parse(os.Args[3:])
+			}
+
+			c.Greet(os.Args[2])
+		default:
+			fmt.Println("Invalid command")
+			menu.Usage()
+			return
+		}
+	} else {
+		menu.Usage()
+		return
+	}
+}
